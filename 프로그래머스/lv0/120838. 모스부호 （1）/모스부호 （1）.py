55c7a265de4a667746b564b44749d01651df85bb
N-jongsik <69664479+N-jongsik@users.noreply.github.com>
2023-07-04 10:36:41 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,15 @@
+def solution(letter):
+    result=[]
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    answer = letter.split()
+    for i in answer:
+        if i in morse:
+            result.append(morse[i])
+            
+    return "".join(result)
