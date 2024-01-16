3d23835b11fc5057f7ef23df26164e0aafcdccb8
N-jongsik <69664479+N-jongsik@users.noreply.github.com>
2024-01-16 15:31:39 +0900

    [level Lv.0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/Lv.0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,7 +1,7 @@
 def solution(numlist, n):
     for i in range(len(numlist)):
         numlist[i] = numlist[i]-n
-    result = sorted(numlist,key = lambda x:[abs(x), -x])
+    result = sorted(numlist,key = lambda x:(abs(x), -x))
     
     for i in range(len(result)):
         result[i] = result[i]+n
