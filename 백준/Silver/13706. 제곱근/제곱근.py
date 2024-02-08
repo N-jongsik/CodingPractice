345d03753fcd8260c1bac73cec065f1ac560d588
N-jongsik <69664479+N-jongsik@users.noreply.github.com>
2024-02-08 15:16:21 +0900

    [Silver IV] Title: 제곱근, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13706. 제곱근/제곱근.py
+++ 백준/Silver/13706. 제곱근/제곱근.py
@@ -1,13 +1,13 @@
 def binary_search(start,end):
     target = end
-    while True:
+    while start <= end:
         mid = (start + end) // 2
         if (mid**2) == target:
             return mid
         elif (mid **2) < target:
-            start = mid
+            start = mid +1
         else:
-            end = mid
+            end = mid - 1
 
 N = int(input()) 
 print(binary_search(1,N))  
