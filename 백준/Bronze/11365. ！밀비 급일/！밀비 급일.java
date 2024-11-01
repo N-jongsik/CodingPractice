2596452b8b9048b1102b0e2aa194052cb5f55788
N-jongsik <69664479+N-jongsik@users.noreply.github.com>
2024-11-01 15:08:18 +0900

    [Bronze IV] Title: !밀비 급일, Time: 192 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            String str = sc.nextLine();
+            if(str.equals("END")){
+                break;
+            }
+            String [] arr = str.split("");
+
+            for(int i = arr.length-1; i >=0 ; i--){
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+        }
+    }
+}
