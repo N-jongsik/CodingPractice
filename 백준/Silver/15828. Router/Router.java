fd48d407e35b346dd5dd995ed336bbab0141c003
Nam Jongsik <69664479+N-jongsik@users.noreply.github.com>
2025-03-16 19:14:45 +0900

    [Silver IV] Title: Router, Time: 892 ms, Memory: 44268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15828. Router/Router.java
+++ 백준/Silver/15828. Router/Router.java
@@ -5,29 +5,32 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());  // 큐 크기 N 입력
 
         Queue<Integer> q = new LinkedList<>();
 
         while(true){
             int k = Integer.parseInt(br.readLine());
 
-            if(k == -1){
+            if(k == -1){  // 종료 조건
                 break;
-            }else if(k == 0){
+            } else if(k == 0){  // 큐에서 값 제거
                 q.poll();
-            }else{
-                q.offer(k);
+            } else {
+                if(q.size() < N){  // 큐의 크기가 N보다 작은 경우에만 추가
+                    q.offer(k);
+                }
             }
         }
 
-        if(q.size() > N){
+        // 큐의 크기가 0인 경우
+        if(q.isEmpty()){
             System.out.println("empty");
-        }else{
+        } else {
+            // 큐의 값을 출력
             for(int item : q){
-                System.out.print(item+" ");
+                System.out.print(item + " ");
             }
         }
-        
     }
 }
