63a5be87a35d19f8ce2c034c3bac38485513dd9a
Nam Jongsik <69664479+N-jongsik@users.noreply.github.com>
2025-04-07 23:53:26 +0900

    [level 1] Title: 모의고사, Time: 0.83 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int [] score = new int[3];
+        
+        int [] one = {1,2,3,4,5};
+        int [] two = {2,1,2,3,2,4,2,5};
+        int [] three = {3,3,1,1,2,2,4,4,5,5};
+        
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == one[i % one.length]) score[0]++;
+            if(answers[i] == two[i % two.length]) score[1]++;
+            if(answers[i] == three[i % three.length]) score[2]++;
+        
+        }
+        
+        int maxScore = Math.max(score[0],Math.max(score[1],score[2]));
+        
+        List<Integer> lst = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++){
+            if(score[i] == maxScore) lst.add(i+1);
+        }
+        
+        int[] answer = new int [lst.size()];
+        
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = lst.get(i);
+        }
+        return answer;
+    }
+}
