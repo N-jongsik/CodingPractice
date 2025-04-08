5a16922688c55606b36b437344d4cddf1d9c417f
Nam Jongsik <69664479+N-jongsik@users.noreply.github.com>
2025-04-08 23:45:17 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int node;
-    static int line;
-    static int [][] graph;
-    static boolean [] check;
-    static int count = 0;
-    
+public class Main{
+    static boolean [] visited;
+    static List<List<Integer>> lst;
+    static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        node = Integer.parseInt(br.readLine());
-        line = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        graph = new int[node+1][node+1];
+        lst = new ArrayList<>();
 
-        for(int i = 0; i < line; i++){
+        for(int i = 0; i <= N; i++){
+            lst.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < M; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            graph[a][b] = graph[b][a] = 1;
+            lst.get(a).add(b);
+            lst.get(b).add(a);
         }
 
-        check = new boolean[node+1];
+        visited = new boolean[N+1];
 
         dfs(1);
-        System.out.println(count - 1);
-        
+
+        System.out.println(answer -1);
     }
-
-    public static void dfs(int start){
-        check[start] = true;
-        count++;
-
-        for(int i = 0; i <= node; i++){
-            if(graph[start][i] == 1 && !check[i]){
-                dfs(i);
+    public static void dfs(int num){
+        visited[num] = true;
+        answer ++;
+        for(int next : lst.get(num)){
+            if(!visited[next]){
+                visited[next] = true;
+                dfs(next);
             }
         }
     }
