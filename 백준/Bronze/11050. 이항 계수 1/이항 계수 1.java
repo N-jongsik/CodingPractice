b0976256093c606ce2f1527a45ee301d36d7d147
Nam Jongsik <69664479+N-jongsik@users.noreply.github.com>
2025-04-15 09:42:58 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int answer = factorial(N) / (factorial(K) * factorial(N-K));
+        System.out.println(answer);
+    }
+
+    static int factorial(int num){
+        int result = 1;
+        for(int i = 1; i <= num; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
